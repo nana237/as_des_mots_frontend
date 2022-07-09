b35a99a7aa154fd1b14f95e197e0b3f63fecb8bd
Boris Nana <boris.nana@news.2easyx.com>
2022-07-09 15:52:44 +0200

    header

--- src/app/pages/auth/connexion/connexion.module.ts
+++ src/app/pages/auth/connexion/connexion.module.ts
@@ -8,6 +8,7 @@ import { ConnexionPageRoutingModule } from './connexion-routing.module';
 
 import { ConnexionPage } from './connexion.page';
 import { FooterModule } from '../../../components/pl-part/footer/footer.module';
+import { HeaderModule } from '../../../components/pl-part/header/header.module';
 
 @NgModule({
   imports: [
@@ -16,7 +17,8 @@ import { FooterModule } from '../../../components/pl-part/footer/footer.module';
     IonicModule,
     ConnexionPageRoutingModule,
     ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
-    FooterModule
+    FooterModule,
+    HeaderModule
   ],
   declarations: [ConnexionPage]
 })
