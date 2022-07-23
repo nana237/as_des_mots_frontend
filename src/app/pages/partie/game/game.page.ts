e4c364836fb4e64f753a47352084583cad807750
Boris Nana <boris.nana@news.2easyx.com>
2022-07-23 06:52:34 +0200

    speechSynthesisVoice

--- src/app/pages/partie/game/game.page.ts
+++ src/app/pages/partie/game/game.page.ts
@@ -1,4 +1,14 @@
+/* eslint-disable @typescript-eslint/type-annotation-spacing */
+/* eslint-disable no-var */
+/* eslint-disable @typescript-eslint/dot-notation */
+/* eslint-disable @typescript-eslint/quotes */
+/* eslint-disable max-len */
 import { Component, OnInit } from '@angular/core';
+// import { waTextToSpeech, waTextToSpeechPaused, waTextToSpeechEnd, waUtterance } from '@ng-web-apis/speech';
+
+interface RecommendedVoices {
+	[key: string]: boolean;
+}
 
 @Component({
   selector: 'app-game',
@@ -7,9 +17,166 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamePage implements OnInit {
 
-  constructor() { }
+  // text='salut';
+  public sayCommand: string;
+	public recommendedVoices: RecommendedVoices;
+	public rates: number[];
+	public selectedRate: number;
+	public selectedVoice: SpeechSynthesisVoice | null;
+	public text: string;
+	public voices: SpeechSynthesisVoice[];
+
+  constructor() {
+    this.voices = [];
+		this.rates = [ .25, .5, .75, 1, 1.25, 1.5, 1.75, 2 ];
+		this.selectedVoice = null;
+		this.selectedRate = 1;
+		// Dirty Dancing for the win!
+		// this.text = "Me? ... I’m scared of everything. I’m scared of what I saw, of what I did, of who I am. And most of all, I’m scared of walking out of this room and never feeling the rest of my whole life ... the way I feel when I’m with you.";
+		this.text = "Bonjour \n Banane \n Salut \n As des Mots \n Prononciation \n Apprentissage \n étudier \n Décourvire \n s'amuser \n évoluer.";
+		this.sayCommand = "";
+
+		// These are "recommended" in so much as that these are the voices that I (Ben)
+		// could understand most clearly.
+		this.recommendedVoices = Object.create( null );
+		this.recommendedVoices[ "Alex" ] = true;
+		this.recommendedVoices[ "Alva" ] = true;
+		this.recommendedVoices[ "Damayanti" ] = true;
+		this.recommendedVoices[ "Daniel" ] = true;
+		this.recommendedVoices[ "Fiona" ] = true;
+		this.recommendedVoices[ "Fred" ] = true;
+		this.recommendedVoices[ "Karen" ] = true;
+		this.recommendedVoices[ "Mei-Jia" ] = true;
+		this.recommendedVoices[ "Melina" ] = true;
+		this.recommendedVoices[ "Moira" ] = true;
+		this.recommendedVoices[ "Rishi" ] = true;
+		this.recommendedVoices[ "Samantha" ] = true;
+		this.recommendedVoices[ "Tessa" ] = true;
+		this.recommendedVoices[ "Veena" ] = true;
+		this.recommendedVoices[ "Victoria" ] = true;
+		this.recommendedVoices[ "Yuri" ] = true;
+  }
+
+  public demoSelectedVoice() : void {
+
+		if ( ! this.selectedVoice ) {
+
+			console.warn( "Expected a voice, but none was selected." );
+			return;
+
+		}
+
+		var demoText = "Best wishes and warmest regards.";
+
+		this.stop();
+		this.synthesizeSpeechFromText( this.selectedVoice, this.selectedRate, demoText );
+
+	}
+
 
   ngOnInit() {
+    this.voices = speechSynthesis.getVoices();
+		this.selectedVoice = ( this.voices[ 0 ] || null );
+		this.updateSayCommand();
+
+		// The voices aren't immediately available (or so it seems). As such, if no
+		// voices came back, let's assume they haven't loaded yet and we need to wait for
+		// the "voiceschanged" event to fire before we can access them.
+		if ( ! this.voices.length ) {
+
+			speechSynthesis.addEventListener(
+				"voiceschanged",
+				() => {
+
+					this.voices = speechSynthesis.getVoices();
+          // this.selectedVoice = this.voices[this.voices.indexOf()];
+					this.selectedVoice = ( this.voices[ 0 ] || null );
+					this.updateSayCommand();
+
+				}
+			);
+
+		}
   }
+
+  onEnd(){
+    console.log('reading finish');
+  }
+
+  public speak() : void {
+
+		if ( ! this.selectedVoice || ! this.text ) {
+
+			return;
+
+		}
+
+		this.stop();
+		this.synthesizeSpeechFromText( this.selectedVoice, this.selectedRate, this.text );
+
+	}
+
+
+	// I stop any current speech synthesis.
+	public stop() : void {
+
+		if ( speechSynthesis.speaking ) {
+
+			speechSynthesis.cancel();
+
+		}
+
+	}
+
+
+	// I update the "say" command that can be used to generate the a sound file from the
+	// current speech synthesis configuration.
+	public updateSayCommand() : void {
+
+		if ( ! this.selectedVoice || ! this.text ) {
+
+			return;
+
+		}
+
+		// With the say command, the rate is the number of words-per-minute. As such, we
+		// have to finagle the SpeechSynthesis rate into something roughly equivalent for
+		// the terminal-based invocation.
+		var sanitizedRate = Math.floor( 200 * this.selectedRate );
+		var sanitizedText = this.text
+			.replace( /[\r\n]/g, " " )
+			.replace( /(["'\\\\/])/g, "\\$1" )
+		;
+
+		this.sayCommand = `say --voice ${ this.selectedVoice.name } --rate ${ sanitizedRate } --output-file=demo.aiff "${ sanitizedText }"`;
+
+	}
+
+	// ---
+	// PRIVATE METHODS.
+	// ---
+
+	// I perform the low-level speech synthesis for the given voice, rate, and text.
+	private synthesizeSpeechFromText(
+		voice: SpeechSynthesisVoice,
+		rate: number,
+		text: string
+		) : void {
+
+		var utterance = new SpeechSynthesisUtterance( text );
+		utterance.voice = this.selectedVoice;
+		utterance.rate = rate;
+		utterance.rate = 0.75;
+
+		speechSynthesis.speak( utterance );
+
+	}
+
+  //  Microsoft Hortense - French (France)
+  // Google UK English Female
+
+
+
+
 
 }
